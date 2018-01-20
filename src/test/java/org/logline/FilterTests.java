c5e285ffa06eec5e2a1dd90718ab7a1b7c0bc599
Yinon Sharifi <yinon.sharifi@oracle.com>
2018-01-20 21:40:23 +0200

    remove redundant tests

--- src/test/java/org/logline/FilterTests.java
+++ src/test/java/org/logline/FilterTests.java
@@ -1,92 +1,41 @@
 package org.logline;
 
-import static org.logline.LogLineConfiguration.on;
+import java.util.Arrays;
+import java.util.Collection;
 
-import java.util.concurrent.CountDownLatch;
-
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.logline.LogLineConfiguration.ILoggingEventFilterWrapper;
-import org.slf4j.Logger;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
 
 /**
  * @author Yinon Sharifi
  */
 
+@RunWith(Parameterized.class)
 public class FilterTests {
-	public static Logger log = LoggerFactory.getLogger(FilterTests.class);
+
+	private ILogger logger;
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		final Logger logback = (Logger) LoggerFactory.getLogger(ActionTests.class);
+		final org.apache.log4j.Logger log4j = org.apache.log4j.Logger.getLogger(ActionTests.class);
+
+		return Arrays.asList(new Object[][] { { new LogbackLogger(logback) }, { new Log4jLogger(log4j) } });
+	}
+
+	public FilterTests(ILogger logger) {
+		this.logger = logger;
+	}
 
 	@Before
 	public void clearConfiguration() {
-		log.info("Clearing logline configuration");
+		logger.info("Clearing logline configuration");
 		LogLineConfiguration.clear();
-	}
-
-	@Test
-	public void testDelayAction() {
-		long delayMs = 350;
-
-		onLogLine("foo").delay(delayMs);
-		long start = System.currentTimeMillis();
-
-		log.info("foo");
-		log.info("bar");
-
-		long duration = System.currentTimeMillis() - start;
-		Assert.assertTrue("Expected at least a delay of " + delayMs + " ms",
-				duration >= delayMs && duration < delayMs + 20);
-	}
-
-	@Test
-	public void testNoDelayAction() {
-		long start = System.currentTimeMillis();
-
-		log.info("foo");
-		log.info("bar");
-
-		long duration = System.currentTimeMillis() - start;
-		Assert.assertTrue("Expected no delay", duration >= 0 && duration < 10);
-	}
-
-	@Test
-	public void testAsyncDelayAction() throws InterruptedException {
-		long delayMs = 350;
-
-		onLogLine("foo").delay(delayMs);
-		CountDownLatch latch = new CountDownLatch(1);
-
-		long start = System.currentTimeMillis();
-
-		new Thread(() -> {
-			log.info("foo");
-			latch.countDown();
-		}).start();
-
-		latch.await();
-
-		long duration = System.currentTimeMillis() - start;
-		Assert.assertTrue("Expected at least a delay of " + delayMs + " ms",
-				duration >= delayMs && duration < delayMs + 20);
-	}
-
-	@Test
-	public void testExceptionAction() {
-		String exceptionMessage = "Dummy exception";
-		onLogLine("foo").throwException(new IllegalStateException(exceptionMessage));
-		try {
-			log.info("foo");
-		} catch (IllegalStateException e) {
-			Assert.assertEquals(e.getMessage(), exceptionMessage);
-			return;
-		}
-
-		Assert.fail("Expected exception to be thrown");
-	}
-
-	private ILoggingEventFilterWrapper onLogLine(String string) {
-		return on(x -> x.getFormattedMessage().contains(string));
 	}
 
 }
